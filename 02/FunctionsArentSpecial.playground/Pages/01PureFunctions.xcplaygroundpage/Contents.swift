6dfd04ccbee1d259c61090a7281cd2a99a4f5816
Daniel Steinberg <daniel@dimsumthinking.com>
2020-04-15 16:14:15 -0400

    re-ordered ch 2

--- 02/FunctionsArentSpecial.playground/Pages/01PureFunctions.xcplaygroundpage/Contents.swift
+++ 02/FunctionsArentSpecial.playground/Pages/01PureFunctions.xcplaygroundpage/Contents.swift
@@ -1,15 +1,9 @@
 //: [Previous](@previous)
-
-var result = freshDeck
-
 let queenOfHearts = Card(.queen,
                            of: .hearts)
 
 let fourOfSpades = Card(.four,
                         of: .spades)
-
-import PlaygroundSupport
-PlaygroundPage.current.setLiveView(DeckView(of: result))
 
 //: [Next](@next)
 
